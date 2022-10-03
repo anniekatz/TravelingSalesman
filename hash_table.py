dd6b47d9da60e0ff0101a1b936865964632a4bd8
Annie Katz <akatz.dev@gmail.com>
2022-10-03 13:54:21 -0400

    updated hash table class

--- hash_table.py
+++ hash_table.py
@@ -1,1 +1,53 @@
 # Manual Hash Table implementation
+
+class HashTable:
+    # Constructor for hash table class
+    # Initializes hash table with an optional parameter for capacity
+    def __init__(self, capacity=40):
+        self.table = [] * capacity
+
+    # Use hash function to get key (bucket)
+    def _hash_func(self, key):
+        hash_bucket = int(key) % len(self.table)
+        return hash_bucket
+
+    # Insert method for hash table class
+    # Insert new package
+    def insert(self, key, val):
+        hash_key = self._hash_func(key)
+        kv_pair = [key, val]
+
+        if self.table[hash_key] is None:
+            self.table[hash_key] = list([kv_pair])
+            return True
+        else:
+            for kv in self.table[hash_key]:
+                if kv[0] == key:
+                    kv[1] = val
+                    return True
+            self.table[hash_key].append(kv_pair)
+            return True
+
+    # Update method for hash table class
+    # Update existing package
+    def update(self, key, val):
+        hash_key = self._hash_func(key)
+        if self.table[hash_key] is not None:
+            for kv in self.table[hash_key]:
+                if kv[0] == key:
+                    kv[1] = val
+                    return True
+        else:
+            print('Error updating packages. Key: ' + key)
+
+    # Delete method for hash table class
+    # Delete existing package
+    def delete(self, key):
+        hash_key = self._hash_func(key)
+        if self.table[hash_key] is None:
+            print('Key ' + key + ' not found')
+            return False
+        for kv in range(0, len(self.table[hash_key])):
+            if self.table[hash_key][kv][0] == key:
+                self.table[hash_key].pop(kv)
+                return True
